854afc1d2632d72562fe91f1f1dc5ca6a625e90f
Kristopher <kristopher_matic@hotmail.com>
2022-07-15 20:43:16 -0700

    operate function to implement operators

--- script.js
+++ script.js
@@ -12,7 +12,20 @@ function multiply(a , b) {
 
 function divide(a , b) {
     if (b == 0) {
-        return "OOPS";
+        return 'OOPS';
     }
     return a / b;
+}
+
+function operate(operator, a, b) {
+    switch (operator) {
+        case 'add':
+            return add(a, b);
+        case 'subtract':
+            return subtract(a, b);
+        case 'multiply':
+            return multiply(a, b);
+        case 'divide':
+            return divide(a, b);
+    }
 }
