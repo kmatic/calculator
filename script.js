afcdbcc1c21dd0980e5c15aa4bf513403e0f11b8
Kristopher <kristopher_matic@hotmail.com>
2022-07-19 11:58:03 -0700

    finish calculator functionality

--- script.js
+++ script.js
@@ -1,19 +1,77 @@
 const numberBtn = document.querySelectorAll('.number');
 const operatorBtn = document.querySelectorAll('.operator');
-const display = document.querySelector('.display');
+const current = document.querySelector('.current');
+const previous = document.querySelector('.previous');
+const clearBtn = document.querySelector('#clear');
+const deleteBtn = document.querySelector('#delete');
+const equalsBtn = document.querySelector('#equals');
 
-let displayVal
+let displayVal = current.textContent;
+let operatorVal = ''; 
+let firstNum = '';
+let secondNum = '';
+let result = '';
+
+deleteBtn.addEventListener('click', () => deleteDisplay());
+clearBtn.addEventListener('click', () => clearDisplay());
+equalsBtn.addEventListener('click', () => calculate());
 
 numberBtn.forEach(button => {
     button.addEventListener('click', () => updateDisplay(button.textContent));
 });
 
+operatorBtn.forEach(button => {
+    button.addEventListener('click', () => setOperator(button.textContent));
+})
+
 function updateDisplay(value) {
-    if (display.textContent == '0') {
-        display.textContent = '';
+    if (current.textContent == '0' || displayVal == '') {
+        current.textContent = '';
     }
-    display.textContent += value;
-    displayVal = display.textContent;
+    current.textContent += value;
+    displayVal = current.textContent;
+}
+
+function setOperator(operator) {
+    if (operatorVal) {
+        calculate();
+    }
+    operatorVal = operator;
+    firstNum = current.textContent;
+    displayVal = '';
+    previous.textContent = `${firstNum} ${operatorVal}`;
+    console.log(operatorVal);
+}
+
+function deleteDisplay() {
+    current.textContent = current.textContent.slice(0, -1);
+    displayVal = current.textContent;
+}
+
+function clearDisplay() {
+    current.textContent = '0';
+    previous.textContent = '';
+    displayVal = current.textContent;
+    operatorVal = '';
+    firstNum = '';
+    secondNum = '';
+    result = '';
+}
+
+function calculate() {
+    // if (displayVal != '') {
+    //     secondNum = current.textContent;
+    // }
+    if (operatorVal == '') {
+        return;
+    }
+    secondNum = current.textContent;
+    result = operate(operatorVal, parseFloat(firstNum), parseFloat(secondNum));
+    current.textContent = result;
+    previous.textContent = `${firstNum} ${operatorVal} ${secondNum} =`;
+    operatorVal = '';
+    secondNum = '';
+    displayVal = '';
 }
 
 function add(a, b) {
@@ -35,16 +93,25 @@ function divide(a , b) {
     return a / b;
 }
 
+function remainder(a , b) {
+    if (b == 0) {
+        return 'OOPS';
+    }
+    return a % b;
+}
+
 function operate(operator, a, b) {
     switch (operator) {
-        case 'add':
+        case '+':
             return add(a, b);
-        case 'subtract':
+        case '-':
             return subtract(a, b);
-        case 'multiply':
+        case '×':
             return multiply(a, b);
-        case 'divide':
+        case '÷':
             return divide(a, b);
+        case '%':
+            return remainder(a, b);
     }
 }
 
